09e902c9d1a982edfdc3de2a9d0220778cd9b221
Tatiana Osipova <otv120780@gmail.com>
2021-12-03 21:29:35 -0500

    Feat: added slider

--- js/main.js
+++ js/main.js
@@ -11,8 +11,8 @@ $(function (){
         slidesToShow: 1,
         slidesToScroll: 1,
         asNavFor: '.heroes__slider-text',
-        prevArrow: <button class="slick-btn slick-prev"><img src="images/arrow left.png" alt="prev"></button>,
-        nextArrow: <button class="slick-btn slick-next"><img src="images/arrow right.png" alt="next"></button>
+        prevArrow: '<button class="slick-btn slick-prev"><img src="images/arrow left.png" alt="prev"></button>',
+        nextArrow: '<button class="slick-btn slick-next"><img src="images/arrow right.png" alt="next"></button>',
     }); 
 
     $('.heroes__slider-text').slick({
@@ -20,6 +20,7 @@ $(function (){
         slidesToScroll: 1,
         asNavFor: '.heroes__slider-img',
         fade: true,
+        arrows: false,
     });
 
 });
